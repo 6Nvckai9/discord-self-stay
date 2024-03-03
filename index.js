bfe8eda70d07a2805a3d892e76ca3e050738c1d3
6Nvckai9 <149785092+6Nvckai9@users.noreply.github.com>
2024-03-03 20:25:16 +0800

    index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ client.on("ready", async () => {
   client.user.setHypeSquad('HOUSE_BRILLIANCE');
   client.user.setPresence({ activities: [r],});
 	const {joinVoiceChannel} = require('@discordjs/voice');
-	const channel = client.channels.cache.get("1213801364866596884"); // voice channel's id
+	const channel = client.channels.cache.get("1213823978884567070"); // voice channel's id
 	if (!channel) return console.log("The channel does not exist !");
 	setInterval(() => {
 			const connection = joinVoiceChannel({
